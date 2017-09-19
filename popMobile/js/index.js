a59db9b4e08a35242511179c9bd52f69b25b721d
shenguotao <sgt_ah@163.com>
2017-09-19 17:00:16 +0800

    popMobile - v0.5

---
+++ popMobile/js/index.js
@@ -0,0 +1,156 @@
+/**
+ * @Date:   2017-09-19T09:49:24+08:00
+ * @Filename: index.js
+ * @Last modified time: 2017-09-19T16:51:35+08:00
+ */
+
+(function($) {
+
+    function onAnimationEnd(element, callback) {
+
+        var verdors = ['a', 'webkitA', 'MozA', 'OA', 'msA'];
+        var endEvents = ['animationend', 'webkitAnimationEnd', 'animationend', 'oAnimationEnd', 'MSAnimationEnd'];
+        var styles = document.createElement('div').style;
+        var event, f;
+        var n = true;
+
+        function handle(e) {
+
+            if (n && e.target === this) {
+                callback && callback();
+                n = false;
+            }
+        }
+
+        if ('onanimationend' in window) {
+            event = 'animationend';
+        } else if ('onwebkitanimationend' in window) {
+            event = 'webkitAnimationEnd';
+        } else {
+            for (var i = 0, len = verdors.length; i < len; i++) {
+                f = verdors[i] + 'nimation';
+                if (f in styles) {
+                    event = endEvents[i];
+                    break;
+                }
+            }
+        }
+
+        if (event && element) {
+            element.removeEventListener(event, handle, false);
+            element.addEventListener(event, handle, false);
+        } else {
+            callback && callback();
+        }
+
+    }
+
+    function popMobileCloseAll(params) {
+
+        var $pop = $('.pop-mobile-container');
+
+        if (params.hideClass) {
+            $pop.removeAttr('style').removeClass(params.hideClass).removeClass('pop-mobile-container');
+        } else {
+            $pop.removeAttr('style').removeClass('pop-mobile-container');
+        }
+
+        $('.mask').remove();
+        $pop = null;
+
+    }
+
+    function popClose(d, m, params) {
+
+        if (params.hideClass) {
+
+            d.removeClass(params.showClass).addClass(params.hideClass);
+            onAnimationEnd(d[0], function() {
+                d.removeAttr('style').removeClass('pop-mobile-container').removeClass(params.hideClass);
+                m.remove();
+                params.after && params.after();
+                d = m = null;
+            });
+        } else {
+            d.removeAttr('style').removeClass('pop-mobile-container');
+            m.remove();
+        }
+
+    }
+
+    function popOpen(params) {
+
+        var $dom = params.dom;
+        var $mask = $('<div class="mask"></div>');
+        var $close = params.close ? $(params.close) : $dom.find('.pop-close');
+        var zindex = params.zindex || 0;
+        var popNum = $('.pop-mobile-container').length;
+        var indep = params.indep || false;
+
+        $dom.css({
+            'position': 'fixed',
+            'left': 0,
+            'top': 0,
+            'right': 0,
+            'bottom': 0,
+            'margin': 'auto',
+            'zIndex': zindex + popNum
+        });
+
+        $mask.css({
+            'position': 'fixed',
+            'left': 0,
+            'top': 0,
+            'right': 0,
+            'bottom': 0,
+            'width': '100%',
+            'height': '100%',
+            'background': 'rgba(0,0,0,.5)',
+            'zIndex': zindex + popNum
+        });
+
+        if (indep) {
+            popMobileCloseAll(params);
+        }
+
+        if (!$dom.is('.pop-mobile-container')) {
+            if (params.showClass) {
+                $dom.show().addClass(params.showClass);
+                onAnimationEnd($dom[0], function() {
+                    params.before && params.before();
+                });
+            } else {
+                $dom.show();
+            }
+            $dom.before($mask);
+        }
+
+        $dom.addClass('pop-mobile-container');
+
+        $close.off('click');
+        $close.on('click', function() {
+            popClose($dom, $mask, params);
+        });
+
+        $dom.off('touchmove');
+        $dom.on('touchmove', function(e) {
+            e.preventDefault();
+        });
+
+    }
+
+    function popMobile(params) {
+
+        if (params.closeAll) {
+            popClose(true)
+            return;
+        }
+
+        new popOpen(params);
+    }
+
+    if (typeof $ === 'function') {
+        $.fn.popMobile = popMobile;
+        $.fn.popMobileCloseAll = popMobileCloseAll;
+    }
+}(jQuery))
